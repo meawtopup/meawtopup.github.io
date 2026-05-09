f9b4754de49c4a39fdc5054e2416324cbb07f075
meawtopup <changplus@windowslive.com>
2026-05-09 09:43:52 +0700

    Update Bookmark-Edit-1.0.js

--- js/Bookmark-Edit-1.0.js
+++ js/Bookmark-Edit-1.0.js
@@ -1,10 +1,7 @@
-/* วิธีใช้ เอาไปใส่ช่อง url ใน bookmark แทนของเดิมได้เลย */
 javascript:(function(){
     if(location.hostname.indexOf('bearbit.org') === -1){
-        /* ถ้าไม่ได้อยู่หน้าเว็บ ให้ไปหน้าแรกก่อน */
         location.href = 'https://bearbit.org/index.php';
     } else {
-        /* ถ้าอยู่หน้าเว็บแล้ว ให้วาร์ปไปหน้า Browse ทันที | cat=10 คือหมวดอนิเมะ */
         location.href = 'https://bearbit.org/viewno18sbx.php?cat=10';
     }
 })();
